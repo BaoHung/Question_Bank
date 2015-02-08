6613e61d8de3d37760a77a57a67886b2c2c7434a
Roux Sparrow <rouxsparrow@gmail.com>
2015-02-09 05:03:31 +0700

    deadline

--- layout/header.php
+++ layout/header.php
@@ -1,23 +1,30 @@
 <div>            
     <!--Header-->
     <ul id="cbp-tm-menu" class="cbp-tm-menu">
-        <li style="width: 24%;">
-            <a href="/Question_Bank">Question Bank</a>
+        <li style="width: 20%;">
+            <a href="#">Question Bank</a>
         </li>
-        <li style="visibility: visible;width: 15%;">
-            <a href="/Question_Bank/question/view.php">Question</a>               
+        <li style="visibility: visible;width: 11%;text-align: center">
+            <a href="#">Question</a>               
         </li>
-        <li style="visibility: visible;width: 15%;">
-            <a href="/Question_Bank/exam/view.php">Exam</a>
+        <li style="visibility: visible;width: 11%;text-align: center">
+            <a href="#">Exam</a>               
         </li>
-        <li style="visibility: visible;width: 15%;">
-            <a href="/Question_Bank/subject/view.php">Subject</a>
+        <li style="visibility: visible;width: 11%;text-align: center">
+            <a href="#">Analysis</a>
+        </li>
+        <li style="visibility: hidden;width: 16%;text-align: center">
+            <a href="#">Control Panel</a>
+            <ul class="cbp-tm-submenu">
+                <li><a href="#" class="cbp-tm-icon-archive">Subject</a></li>
+                <li><a href="#" class="cbp-tm-icon-users">Account</a></li>                    
+            </ul>
         </li>
         <li style="width: 28%;text-align: right">
             <a href="#">Roux Sparrow</a>
             <ul class="cbp-tm-submenu">
-                <li><a href="/Question_Bank/Change_Pass.php" class="cbp-tm-icon-cog">Change password</a></li>
-                <li><a href="/Question_Bank/Login.html" class="cbp-tm-icon-contract">Log out</a></li>                    
+                <li><a href="#" class="cbp-tm-icon-cog">Change password</a></li>
+                <li><a href="#" class="cbp-tm-icon-contract">Log out</a></li>                    
             </ul>
         </li>
     </ul>
