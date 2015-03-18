ab38b663a099167d27dba0bf16f0bff68c465e86
BH <BH@10.20.14.189>
2015-03-18 23:54:11 +0700

    18/3 11:55

---
+++ account/authenticate.php
@@ -0,0 +1,19 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+$Accounts = simplexml_load_file("../xml/Accounts.xml");
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+
+$isCorrectInfo = FALSE;
+
+foreach ($Accounts->children() as $Account) {
+    if ($username == $Account->Email && $password == $Account->Password) {
+        $isCorrectInfo = TRUE;
+        session_start();
+        $_SESSION["accountID"] = $Account['id']->__toString();
+        break;
+    }
+}
+echo json_encode($isCorrectInfo);
---
+++ session.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["accountID"]) && (!empty($_SESSION["accountID"]) || $_SESSION["accountID"] == 0 )) {
+    $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+    var_dump($root);
+
+    $Accounts = simplexml_load_file("$root/xml/Accounts.xml");
+    $Account = NULL;
+    $id = $_SESSION["accountID"];
+    foreach ($Accounts->children() as $A) {
+        if ($A['id'] == $id) {
+            $Account = $A;
+            break;
+        }
+    }
+} else {
+    header('Location: /Login.html');
+}
