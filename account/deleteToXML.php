f7f4c4eb748d481535043970657a013585c92f1a
BH <BH@10.20.14.186>
2015-03-22 23:45:36 +0700

    Forbid remove own account

--- account/deleteToXML.php
+++ account/deleteToXML.php
@@ -1,6 +1,8 @@
 <?php
+session_start();
 
 $id = filter_input(INPUT_POST, 'id');
+$currentAccountID = $_SESSION["accountID"];
 
 $result = array(
     'mode' => 'delete',
@@ -10,16 +12,20 @@ $result = array(
 
 header('Content-Type: application/json; charset=utf-8');
 
-$Accounts = simplexml_load_file("../xml/Accounts.xml");
+if ($id == $currentAccountID) {
+    $result['completed'] = FALSE;
+    $result['message'] = 'You cannot remove your own account.';
+} else {
+    $Accounts = simplexml_load_file("../xml/Accounts.xml");
 
-foreach ($Accounts->children() as $A) {
-    if ($id == $A['id']) {
-        $A['removed'] = '1';
-        break;
+    foreach ($Accounts->children() as $A) {
+        if ($id == $A['id']) {
+            $A['removed'] = '1';
+            break;
+        }
     }
+
+    $result['completed'] = $Accounts->asXML("../xml/Accounts.xml");
+    $result['message'] = 'Account removed successfully';
 }
-
-$result['completed'] = $Accounts->asXML("../xml/Accounts.xml");
-$result['message'] = 'Account removed successfully';
-
 echo json_encode($result);
